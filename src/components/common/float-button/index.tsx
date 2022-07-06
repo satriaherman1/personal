8e6f26adeb0459c31e069ab649186399a432b382
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-07-06 16:09:24 +0700

    add hide navigation

--- src/components/common/float-button/index.tsx
+++ src/components/common/float-button/index.tsx
@@ -1,5 +1,6 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { Box, Flex, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Flex, Icon, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { useState } from "react";
 import "./styles.css";
 
 export default function FloatButton(props: IFloatButton) {
@@ -7,9 +8,19 @@ export default function FloatButton(props: IFloatButton) {
   const { colorMode, toggleColorMode } = useColorMode();
   const bg = useColorModeValue("#ececec99", "#19202799");
   const darkButtonBg = useColorModeValue("gray.700", "#2A4365");
+  const hideButtonBg = useColorModeValue("green.500", "#19202799");
+
+  const [hideFloatButton, setHideFloatButton] = useState<boolean>(false);
+
+  const handleVisibility = () => {
+    setHideFloatButton(!hideFloatButton);
+  };
 
   return (
-    <Flex zIndex={999} padding="10px 20px" gap={3} position="fixed" alignItems="center" bottom="5vh" left="50%" className="float-button-container" background={bg}>
+    <Flex zIndex={999} padding="10px 20px" gap={3} position="fixed" alignItems="center" bottom="5vh" left="50%" className={`float-button-container ${hideFloatButton && "hide"}`} background={bg}>
+      <Box as="button" className="hide-button" bg={hideButtonBg} onClick={handleVisibility}>
+        {hideFloatButton ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </Box>
       {buttonList.map((btn) => (
         <a href={`#${btn.name}`} className={`float-button ${btn.name === active && "active"}`} onClick={btn.action}>
           {btn.children}
