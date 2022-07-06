c442dfcbad2570e91eecff46c5deec4b9f63692f
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-07-06 15:09:21 +0700

    fix portfolio

--- src/pages/Portfolio/detail.tsx
+++ src/pages/Portfolio/detail.tsx
@@ -1,8 +1,8 @@
-import { Container, Divider, Heading, Icon, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Divider, Flex, Heading, Icon, Text, useColorModeValue } from "@chakra-ui/react";
 import Footer from "@src/components/pages/home/footer";
 import HomePortfolio from "@src/components/pages/home/portfolio";
 import PortfolioService from "@src/service/rest/portfolio.service";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaFacebook, FaFacebookF, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
 import ReactMarkdown from "react-markdown";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
@@ -18,6 +18,7 @@ export default function DetailPortfolio() {
   };
 
   const { data } = useQuery(`detail-portfolio/${id}`, getPortfolioData);
+  const textGrayTheme = useColorModeValue("gray.600", "white");
 
   return (
     <>
@@ -30,13 +31,33 @@ export default function DetailPortfolio() {
         <Heading as="h1" fontSize={32} marginTop={5}>
           {data?.data.data.attributes.Title}
         </Heading>
-        <Text marginTop={2} color="gray.600">
+        <Text marginTop={2} color={textGrayTheme}>
           {data?.data.data.attributes.company}
         </Text>
 
         <Divider marginY={5} />
 
         <ReactMarkdown rehypePlugins={[rehypeRaw]} children={data?.data?.data.attributes.content} />
+
+        <Box marginTop={8} marginBottom={10}>
+          <Heading as="h4" fontWeight="500" fontSize={18}>
+            Share Portfolio :{" "}
+          </Heading>
+          <Flex marginTop={3} gap={2}>
+            <Button colorScheme="facebook" size="sm">
+              <Icon as={FaFacebookF} />
+            </Button>
+            <Button colorScheme="twitter" size="sm">
+              <Icon as={FaTwitter} />
+            </Button>
+            <Button colorScheme="pink" size="sm">
+              <Icon as={FaInstagram} />
+            </Button>
+            <Button colorScheme="whatsapp" size="sm">
+              <Icon as={FaWhatsapp} />
+            </Button>
+          </Flex>
+        </Box>
 
         <Divider marginY={3} />
         <Heading as="h2" fontSize={28} marginTop={5}>
