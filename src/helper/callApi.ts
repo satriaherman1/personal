d2d515e95bf2e17f134058c3a38740bb8ca0eb2c
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-07-29 15:13:41 +0700

    fix err

--- src/helper/callApi.ts
+++ src/helper/callApi.ts
@@ -1,7 +1,11 @@
+import environment from "@src/config/environment/environment";
 import axios from "axios";
 
 const callApi = axios.create({
   baseURL: "https://satria-personal-backend.herokuapp.com/api",
+  headers: {
+    Authorization: `Bearer ${environment.TOKEN}`,
+  },
 });
 
 export default callApi;
