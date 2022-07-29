352645500d5b4ff42943be1a70863de020c96295
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-07-29 14:48:34 +0700

    fix error lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Loading from "@src/components/common/loading";
 import theme from "@src/definition/theme";
+import Home from "@src/pages/Home";
+import DetailPortfolio from "@src/pages/Portfolio/detail";
 import { lazy, Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/global.css";
 
-const HomePage = lazy(() => import("./pages/Home"));
-const DetailPortfolio = lazy(() => import("./pages/Portfolio/detail"));
+// const HomePage = lazy(() => import("./pages/Home"));
+// const DetailPortfolio = lazy(() => import("./pages/Portfolio/detail"));
 
 function App() {
   const queryClient = new QueryClient();
@@ -19,7 +21,7 @@ function App() {
         {/* <Suspense fallback={<Loading />}> */}
         <BrowserRouter>
           <Routes>
-            <Route path="/" caseSensitive element={<HomePage />} />
+            <Route path="/" caseSensitive element={<Home />} />
             <Route path="/portfolio/:id" element={<DetailPortfolio />} />
           </Routes>
         </BrowserRouter>
