186b0cacac154e3a222d5f68e3558514810dfff6
satriaherman1 <muhammadsatriaherman@gmail.com>
2024-09-15 12:54:05 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Satria Herman",
   description: "Professional Web Developer",
+  icons: {
+    icon: "/favicon/favicon.png", // Ganti dengan path ikon Anda
+    shortcut: "/favicon/favicon.ico", // Ganti dengan path ikon Anda
+    apple: "/favicon/apple-icon.png", // Ganti dengan path ikon Apple Anda
+  },
 };
 
 export default function RootLayout({
