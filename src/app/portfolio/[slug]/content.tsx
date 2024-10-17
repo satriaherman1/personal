2192906a90ea171695c840fc963723ce94664965
satriaherman1 <muhammadsatriaherman@gmail.com>
2024-10-17 23:21:29 +0700

    update text color

--- src/app/portfolio/[slug]/content.tsx
+++ src/app/portfolio/[slug]/content.tsx
@@ -4,6 +4,8 @@ import { Portfolio } from "@src/app/portfolio/[slug]/page";
 import Navbar from "@src/components/commons/navbar";
 import useStyleRewrite from "@src/utils/hooks/style-rewrite";
 import Image from "next/image";
+import Link from "next/link";
+import { MdArrowBack, MdArrowOutward } from "react-icons/md";
 
 export default function PortfolioContent({
   title,
@@ -42,28 +44,37 @@ export default function PortfolioContent({
 
         <section className="mt-20">
           <h1 className="text-2xl font-semibold">{title}</h1>
-          <span className="text-gray-600 dark:text-gray-300 ">{company}</span>
+          <span className="text-gray-600  dark:text-gray-400 ">{company}</span>
         </section>
+
+        <a
+          href=""
+          className="btn btn-primary btn-small inline-flex items-center gap-x-2 mt-4"
+        >
+          View Website <MdArrowOutward />
+        </a>
 
         <div className="shadow-md border px-9 py-6 rounded-lg my-8 max-w-[400px]">
           <div className="flex justify-between">
             <section>
               <h2 className="t uppercase font-bold tracking-wider">Client</h2>
-              <span className="text-gray-600">{client}</span>
+              <span className="text-gray-600  dark:text-gray-400">
+                {client}
+              </span>
             </section>
             <section>
               <h2 className="t uppercase font-bold tracking-wider">Role</h2>
-              <span className="text-gray-600">{role}</span>
+              <span className="text-gray-600 dark:text-gray-400">{role}</span>
             </section>
             <section>
               <h2 className="t uppercase font-bold tracking-wider">Year</h2>
-              <span className="text-gray-600">{year}</span>
+              <span className="text-gray-600 dark:text-gray-400">{year}</span>
             </section>
           </div>
         </div>
 
         <div
-          className="mt-3 html-rendered text-gray-600"
+          className="mt-3 html-rendered text-gray-600 dark:text-gray-400"
           dangerouslySetInnerHTML={{ __html: simpleDescription }}
         ></div>
 
@@ -99,6 +110,13 @@ export default function PortfolioContent({
             </button>
           </div>
         </section> */}
+
+        <Link
+          href="/"
+          className="btn btn-outlined inline-flex gap-x-2 mt-8 items-center btn-small "
+        >
+          <MdArrowBack /> Go Back
+        </Link>
 
         <div className="h-20"></div>
       </main>
